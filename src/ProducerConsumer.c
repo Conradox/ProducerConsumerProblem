57e1911c0361d7abd60d000904097c12fccc3eb0
Pedro <pedrooconradoo@gmail.com>
2021-04-08 22:48:30 -0300

    Adding functions with behaviors of entities and a function to print buffer

--- src/ProducerConsumer.c
+++ src/ProducerConsumer.c
@@ -35,3 +35,92 @@ Consumer* create_consumer(int batch_size, Buffer * buffer)
     return new_comsumer;   
 }
 
+void * producer_behavior(void * arg)
+{
+    Producer * me = (Producer *) arg;
+    int position = 0;
+    int res;
+    while(1)
+    {
+        sem_wait(&me->buffer->is_occupied);
+        for(int i = 0; i < me->batch_size; i++)
+        {
+            res = sem_trywait(&me->buffer->is_full);
+            if(res == 0)
+            {
+                me->buffer->storage[position] = (int) rand() % 100;
+                position = (int) (position + 1) % me->buffer->size;
+                sem_post(&me->buffer->is_void);
+            }else
+            {
+                break;
+            }
+        }
+        #ifdef DEBUG
+        print_buffer(me->buffer, "Producer");
+        #endif
+        sem_post(&me->buffer->is_occupied);
+        #ifdef DELAY
+            sleep(DELAY);
+        #endif
+    }
+}
+
+void * consumer_behavior(void * arg)
+{
+    Consumer * me = (Consumer *) arg;
+    int position = 0;
+    int res;
+    while(1)
+    {
+        sem_wait(&me->buffer->is_occupied);
+        for(int i = 0; i < me->batch_size; i++)
+        {
+            res = sem_trywait(&me->buffer->is_void);
+            if(res == 0)
+            {
+                me->buffer->storage[position] = -1;
+                position = (int) (position + 1) % me->buffer->size;
+                sem_post(&me->buffer->is_full);
+            }else
+            {
+                break;
+            }
+        }
+        #ifdef DEBUG
+        print_buffer(me->buffer, "Consumer");
+        #endif
+        sem_post(&me->buffer->is_occupied);
+        #ifdef DELAY
+            sleep(DELAY);
+        #endif
+    }
+}
+
+void print_buffer(Buffer * buffer, char * str)
+{
+    int i = 0;
+    if(buffer->size > 0)
+    {
+        if(buffer->storage[i] == -1)
+        {
+            printf("|  |");
+        }else{
+            printf("%d|", buffer->storage[i]);
+        }
+        for(i = 1; i < buffer->size; i++)
+        {
+            if(buffer->storage[i] == -1)
+            {
+                printf("  |");
+            }else{
+                printf("%d|", buffer->storage[i]);
+            }
+        }
+    }
+    int void_value;
+    int full_value;
+    sem_getvalue(&buffer->is_void, &void_value);
+    sem_getvalue(&buffer->is_full, &full_value);
+    printf(" <- %s| VOID %d| FULL %d|\n", str, void_value, full_value);
+}
